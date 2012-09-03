021a4905df761e8f2cefd9d67333989b18d15f32
Alexander Astafurov <gogromat@fastmail.fm>
2012-09-02 23:28:14 -0400

    Add a full_title helper

---
+++ example_user.rb
@@ -0,0 +1,17 @@
+class ExampleUser
+  #getter and setter methods for name, email instance variables
+  attr_accessor :name, :email
+
+  def initialize(attributes = {})
+    @name  = attributes[:name]
+    @email = attributes[:email]
+  end
+
+  def formatted_email
+    "#{@name} <#{@email}>"
+  end
+
+
+
+
+end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -10,11 +10,22 @@ describe "Static pages" do
                                 :text => 'Sample App')
     end
 
-    it "should have the title 'Home'" do
+    it "Should have the base title" do
       visit '/static_pages/home'
       page.should have_selector('title',
-                                :text => "Ruby on Rails Tutorial Sample App | Home")
+                                :text => 'Ruby on Rails Tutorial Sample App')
     end
+
+    it "Should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title',
+                                    :text => '| Home')
+    end
+    #    it "should have the title 'Home'" do
+    #      visit '/static_pages/home'
+    #      page.should have_selector('title',
+    #                                :text => "Ruby on Rails Tutorial Sample App | Home")
+    #    end
   end
 
   describe "Help page" do
